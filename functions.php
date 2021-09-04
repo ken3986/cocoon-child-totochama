cd4f1b187faa9cdffd8b7d3dcc035fa68f36d24f
ken3986 <naka_ken8686@yahoo.co.jp>
2021-09-04 15:37:52 +0900

    更新確認機能を実装

--- functions.php
+++ functions.php
@@ -4,3 +4,12 @@ get_template_part('lib/parent-category-label');
 get_template_part('lib/excerpt-br');
 get_template_part('lib/widget-area-index-bottom-plus');
 get_template_part('lib/categories-term-order');
+
+// 更新の確認
+require 'lib/plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+  'https://github.com/ken3986/cocoon-child-totochama',
+  __FILE__,
+  'cocoon-child-totochama'
+);
+$myUpdateChecker -> getVcsApi() -> enableReleaseAssets();
